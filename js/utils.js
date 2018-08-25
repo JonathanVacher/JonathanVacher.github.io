acbbb7595fbc7f1b90bcc9b2607c3b5370540178
jonathan <jonathan.vacher@einstein.yu.edu>
2018-08-25 17:00:08 -0400

    update-sidebar

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,28 @@
 function sidebar(){
-	document.write('<div id="sidebar"> <div id="sidebar-fixed"> <img src="myphoto.jpg" title="my photo" id="my_photo" /> <h3>Contact</h3> <p> Albert Einstein College of Medicine, <br /> Block Research Pavilion, <br /> Department of Systems and Computational Biology, <br /> 1301 Morris Park Avenue, <br /> 10461 Bronx, NY <br /> USA <br /> firstname.lastname@einstein.yu.edu<br /> <a href="#">https://jonathanvacher.github.io/</a></p> <h3>Acknowledgements</h3> <p> Thanks to Manon Esnouf for the amazing photo ! </p> </div></div>');	
+	document.write( '<div id="sidebar">' +
+			'<img src="myphoto.jpg" title="my photo" id="my_photo" />' +
+			'<h3>Contact</h3>' +
+			'<p> Albert Einstein College of Medicine, <br />' +
+			'Block Research Pavilion, <br />' +
+			'Department of Systems and Computational Biology, <br />' +
+			'1301 Morris Park Avenue, <br />' +
+			'10461 Bronx, NY <br />' +
+			'USA <br />' +
+			'firstname.lastname@einstein.yu.edu<br />' +
+			'<a href="#">https://jonathanvacher.github.io/</a></p>' +
+			'<h3>Acknowledgements</h3>' +
+			'<p> Thanks to Manon Esnouf for the amazing photo ! </p> </div></div>');	
 		}
 
 
 function banner(){
-	document.write('<div id="banner"> <div id="name"> <h1>Jonathan Vacher</h1> <p>PhD</p> <p>Department of Systems and Computational Biology </p> <p>Albert Einstein College of Medicine, New York</p> </div> <div id="welc"> <h1>Welcome</h1> </div> </div>');	
+	document.write( '<div id="banner">' +
+			'<div id="name">' +
+			'<h1>Jonathan Vacher</h1>' +
+			'<p>PhD</p>' + 
+			'<p>Department of Systems and Computational Biology </p>' +
+ 			'<p>Albert Einstein College of Medicine, New York</p>' + 
+			'</div> <div id="welc"> <h1>Welcome</h1> </div> </div>');	
 		}
 
 function news(title,text){
